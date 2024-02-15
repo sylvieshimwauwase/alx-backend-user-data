77a6c611267bd1cc84e3d6b90cfae642e4ca8380
sylvieshimwauwase <slvshimwa@gmail.com>
2024-02-15 17:08:38 +0200

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,8 +56,9 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        found_users = User.search({'email': user_email})
-        if found_users is None:
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
             return None
 
         for user in found_users:

b67ebcbdfb5542e51ff025abfa345b3c20589a84
sylvieshimwauwase <slvshimwa@gmail.com>
2024-02-20 07:57:56 +0200

    updating session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,4 +6,15 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """SessionAuth class"""
-    pass
+
+    User_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.User_id_by_session_id[session_id] = user_id
+        return session_id

314dafc71680d452c930c3dabbce86e1b506281e
sylvieshimwauwase <slvshimwa@gmail.com>
2024-07-22 13:55:34 +0200

    adding changes on authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,55 +1,73 @@
 #!/usr/bin/env python3
-"""db module"""
+"""
+BD class
+"""
 
-from user import Base, User
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-import logging
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
 
 DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
-logging.basicConfig()
-logging.getLogger('sqlalchemy').setLevel(logging.WARNING)
-
 
 class DB:
-    """DB class"""
 
     def __init__(self):
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Memoized session object
-        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add user"""
-        if not email or not hashed_password:
-            raise ValueError("Email and hashed_password are required.")
+        """add user to database
 
+        Args:
+            email (string): email of user
+            hashed_password (string): password of user
+        Returns:
+            User: user created
+        """
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
         session.commit()
-
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find user by"""
+        """find user by some arguments
+
+        Returns:
+            User: user found or raise error
+        """
         user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
+        if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user
+
+        Args:
+            user_id (int): id of user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, val in kwargs.items():
+            if key not in DATA:
+                raise ValueError
+            setattr(user, key, val)
+        self._session.commit()
+        return None
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,30 +1,74 @@
 #!/usr/bin/env python3
 """
-Main file
+Main Module
 """
 from db import DB
 from user import User
-
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 
-my_db = DB()
+def register_user(email: str, password: str) -> None:
+    """register_user"""
+    assert True
+    return
 
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
 
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
+def log_in_wrong_password(email: str, password: str) -> None:
+    """log_in_wrong_password"""
+    assert True
+    return
 
-try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
 
-try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
+def log_in(email: str, password: str) -> str:
+    """log_in"""
+    assert True
+    return ("")
+
+
+def profile_unlogged() -> None:
+    """profile_unlogged"""
+    assert True
+    return
+
+
+def profile_logged(session_id: str) -> None:
+    """profile_logged"""
+    assert True
+    return
+
+
+def log_out(session_id: str) -> None:
+    """log_out"""
+    assert True
+    return
+
+
+def reset_password_token(email: str) -> str:
+    """reset_password_token"""
+    assert True
+    return ("")
+
+
+def update_password(reset_token: str, new_password: str) -> None:
+    """update_password"""
+    assert True
+    return
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token)
+    log_in(EMAIL, NEW_PASSWD)
